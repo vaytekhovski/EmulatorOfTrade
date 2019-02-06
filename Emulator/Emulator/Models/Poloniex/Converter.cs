b70422d2cf074227ffc36c993cf9bfbf76ee4b77
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-02-06 15:57:46 +0300

    upd3

---
+++ Emulator/Emulator/Models/Poloniex/Converter.cs
@@ -0,0 +1,21 @@
+﻿namespace QuickType
+{
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+}
---
+++ Emulator/Emulator/Models/Poloniex/Methods/ChartData.cs
@@ -0,0 +1,40 @@
+﻿namespace QuickType
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+
+    public partial class ChartData
+    {
+        [JsonProperty("date")]
+        public long Date { get; set; }
+
+        [JsonProperty("high")]
+        public double High { get; set; }
+
+        [JsonProperty("low")]
+        public double Low { get; set; }
+
+        [JsonProperty("open")]
+        public double Open { get; set; }
+
+        [JsonProperty("close")]
+        public double Close { get; set; }
+
+        [JsonProperty("volume")]
+        public double Volume { get; set; }
+
+        [JsonProperty("quoteVolume")]
+        public double QuoteVolume { get; set; }
+
+        [JsonProperty("weightedAverage")]
+        public double WeightedAverage { get; set; }
+
+
+    }
+
+    public partial class ChartData
+    {
+        public static List<ChartData> FromJson(string json) => JsonConvert.DeserializeObject<List<ChartData>>(json, QuickType.Converter.Settings);
+    }
+}
