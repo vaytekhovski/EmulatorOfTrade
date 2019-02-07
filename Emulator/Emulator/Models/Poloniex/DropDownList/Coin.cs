ac5dc0547e738d4a53a2d0449b8b9408ef181c32
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-02-07 11:33:03 +0300

    добавлен выбор валюты

---
+++ Emulator/Emulator/Models/Poloniex/DropDownList/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Emulator.Models.Poloniex.DropDownList
+{
+    public class Coin
+    {
+        public Coins Name { get; set; }
+    }
+
+    public enum Coins
+    {
+        BTC, XRP, ETH
+    }
+}
